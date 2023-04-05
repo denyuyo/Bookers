0da88b9acda559df32542a47fca9f8c84ab7154c
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-04-05 11:20:26 +0000

    Inital Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,46 +10,29 @@ class BooksController < ApplicationController
   end
 
   def edit
+     @book = Book.find(params[:id])
   end
   
   def create
-    @book = Book.new(book_params)
-
-      if @book.save
-        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
-        format.json { render :show, status: :created, location: @book }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books'
   end
 
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
-      end
-    end
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path(book.id)  
   end
 
   def destroy
-    @book.destroy
-
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
-  def set_book
-      @book = Book.find(params[:id])
-  end
   def book_params
-      params.require(:book).permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
